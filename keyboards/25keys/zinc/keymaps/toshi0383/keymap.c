e4433a4cfb1ce921912272af360a871940b9e650
toshi0383 <t.suzuki326@gmail.com>
2024-08-03 15:27:04 +0900

    Add RCTL(KC_RSFT) in Raise layer

--- keyboards/25keys/zinc/keymaps/toshi0383/keymap.c
+++ keyboards/25keys/zinc/keymaps/toshi0383/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  _______,
     _______, KC_COLN, KC_SCLN, KC_DQT, KC_QUOT, KC_BSLS,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______,  ADJUST, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_LGUI, KC_TRNS,  KC_TRNS, _______, _______, _______, _______, _______
+    _______, _______, _______, RCTL(KC_RSFT), KC_LGUI, KC_TRNS,  KC_TRNS, _______, _______, _______, _______, _______
     ),
 
   [_ADJUST] =  LAYOUT_ortho_4x12(
